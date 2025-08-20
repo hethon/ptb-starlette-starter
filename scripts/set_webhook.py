c46e544d4ea3e746415f8a7c35cba89d0a7d00c1
Henok Tesfamikael <henoktesfamikael16@gmail.com>
2025-08-20 04:22:45 +0300

    fix: add a return statment

--- scripts/set_webhook.py
+++ scripts/set_webhook.py
@@ -6,7 +6,7 @@ from ptb.tg_app import tg_app
 
 async def set_webhook() -> bool:
     """Pass webhook settings to telegram"""
-    await tg_app.bot.set_webhook(
+    return await tg_app.bot.set_webhook(
         url=config.WEBHOOK_URL,
         allowed_updates=[Update.MESSAGE, Update.CALLBACK_QUERY],
         secret_token=config.SECRET_TOKEN,
