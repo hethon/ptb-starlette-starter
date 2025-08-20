357440dc2642379b6ec1db8c39278db8761cc2e3
Henok Tesfamikael <henoktesfamikael16@gmail.com>
2025-08-20 04:41:15 +0300

    check for the existance of WEBHOOK_URL to make the type checker happy

--- scripts/set_webhook.py
+++ scripts/set_webhook.py
@@ -6,6 +6,9 @@ from ptb.tg_app import tg_app
 
 async def set_webhook() -> bool:
     """Pass webhook settings to telegram"""
+    if not config.WEBHOOK_URL:
+        return False
+        
     return await tg_app.bot.set_webhook(
         url=config.WEBHOOK_URL,
         allowed_updates=[Update.MESSAGE, Update.CALLBACK_QUERY],
